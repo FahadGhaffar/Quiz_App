63a5c98307ded1dcf111e110927ed04ba3bca0ed
Fahad Ghaffar <fahad.ghaffar97@gmail.com>
2022-08-27 14:17:12 -0700

    Assignment

---
+++ js/app.js
@@ -0,0 +1,53 @@
+// $('a#first').click(function () {
+//     $('div#container').show();
+// });
+// $('a#last').click(function () {
+//     $('div#container').hide();
+// });
+
+var questions = [
+    {
+        id: 1,
+        question: "What is the full form of RAM ?",
+        answer: "Random Access Memory",
+        options: [
+            "Random Access Memory",
+            "Randomely Access Memory",
+            "run aceapt memory",
+            "none of these",
+        ]
+    },
+    {
+        id: 2,
+        question: "What is the full form of CPU ?",
+        answer: "Central Processing Unit",
+        options: [
+            "Central program Unit",
+            "Central Processing Unit",
+            "Central preload Unit ",
+            "none of these",
+        ]
+    },
+    {
+        id: 3,
+        question: "What is the full form of E-mail?",
+        answer: "Electronic Mail",
+        options: [
+            "Electric Mail",
+            "Elementery Mail",
+            "Electronic Mail",
+            "none of these",
+        ]
+    },
+    {
+        id: 4,
+        question: "What is the full form of RAM ?",
+        answer: "Random Access Memory",
+        options: [
+            "Random Access Memory",
+            "Randomely Access Memory",
+            "run aceapt memory",
+            "none of these",
+        ]
+    },
+]
